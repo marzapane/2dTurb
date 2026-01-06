84eb97402abda69434f459b464e7da0f0bc34c2f
Pierpaolo <pier.bilotto@gmail.com>
2026-01-06 21:19:51 +0100

    Put randomness of the  initial condition in the stream function, instead of potential vorticity

--- QG_main.py
+++ QG_main.py
@@ -1,34 +1,36 @@
 #!/opt/intel/oneapi/intelpython/latest/bin/python
-# #!/usr/bin/env python
+# #!/usr/bin/env python3 -u
 
 import argparse
 import numpy as np
 import po2d_lib as po
     
 def randIC(Energy, Dx, N, topo=None):
-    q = po.random_topography(40, Dx, N)
+    psi_init = po.random_topography(100, Dx, N)
     # q = (q - q[:, N-1::-1]) / 2
     # q = (q - q[N-1::-1, :]) / 2
+    q_init = po.pseudo_laplacian2d(psi_init)/Dx**2
     if topo is None:
-        fluid = po.FluidState(rel_vorticity=q)
+        fluid = po.FluidState(rel_vorticity=q_init)
     else:
-        fluid = po.FluidStateTopography(topo, rel_vorticity=q)
+        fluid = po.FluidStateTopography(topo, rel_vorticity=q_init)
     fluid.qsum20()
-    fluid.streamfunction(0)
     E = fluid.energy()
-    q = fluid.q * np.sqrt(Energy / E)
+    q_init = fluid.q * np.sqrt(Energy / E)
     del fluid
-    return q
+    return q_init
 
 def main(args):
-    simul = po.FluidSimulator(po.random_forcing, adaptive_Dt=True)
-    # simul = po.FluidSimulator(po.zero_forcing, adaptive_Dt=True)
+    # simul = po.FluidSimulator(po.random_forcing, adaptive_Dt=True)
+    simul = po.FluidSimulator(po.zero_forcing, adaptive_Dt=True)
     simul.silent = args.silent
     simul.initialize_run_state()
-    topo = po.gauss_topography(simul.Dx, simul.N)
-    # energy = 100.
-    # fluid = po.FluidStateTopography(topo, simul, rel_vorticity=randIC(energy, simul.Dx, simul.N, topo))
-    fluid = po.FluidStateTopography(topo, simul)
+    # topo = po.gauss_topography(simul.Dx, simul.N)
+    topo = po.random_topography(100, simul.Dx, simul.N)
+    # energy = 1.
+    psi_init = randIC(1., simul.Dx, simul.N, topo)
+    fluid = po.FluidStateTopography(topo, simul, rel_vorticity=psi_init)
+    # fluid = po.FluidStateTopography(topo, simul)
         
     simul.set_physical_param(fluid)
     for t in simul.time_exec:
